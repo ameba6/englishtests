497129d2d842e2f891f118de1eec47315cd1c1b5
cortesa <cortesa@gmail.com>
2016-08-26 14:12:33 +0200

    Repair CSS issues

--- client/pages/MyHome.jsx
+++ client/pages/MyHome.jsx
@@ -9,7 +9,6 @@ export default class MyHome extends Component {
 				<SimpleSlider />
 				<br/>
 				<h1>Welcome to my page</h1>
-				<h2>it looks better under Chrome</h2>
 				<p>Hello, this is my landing page</p>
 			</div>
 		)
